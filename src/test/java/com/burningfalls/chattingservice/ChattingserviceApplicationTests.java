1d29b1a29f4b24db4c308db0b89c0702e200e079
BurningFalls <sherlocky31415@gmail.com>
2023-09-04 14:19:32 +0900

    start project (using start.spring.io)

---
+++ src/test/java/com/burningfalls/chattingservice/ChattingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burningfalls.chattingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
